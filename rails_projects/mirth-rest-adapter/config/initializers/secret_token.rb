3f34dfb5ed2b132f21b22f7fb23cda23acb13888
Raymond Rusk <Raymond.Rusk@shaw.ca>
2013-04-06 17:02:08 -0700

    Build your own Mirth Rest Adapter in 10 Minutes

---
+++ rails_projects/mirth-rest-adapter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MirthRestAdapter::Application.config.secret_token = 'cecc268c7a47feeeb7a3a648d57b09a2c80afb24bbfd2ac2c1ed4cb1b84f283a88e56896b8820ac1a778c374115363983ba08d31bf008181aa51db192c2d28c5'
